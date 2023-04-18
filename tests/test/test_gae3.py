58ab1c9edab03c82bd4a2522481a0c3ada53c92c
Vitaly Bogomolov <mail@vitaly-bogomolov.ru>
2023-04-18 15:41:32 +0400

    step

--- tests/test/test_gae3.py
+++ tests/test/test_gae3.py
@@ -20,3 +20,24 @@ class TestInitTestbed(TestCase):
 
         tester.tear_down()
         assert tester.gae_testbed is None
+
+
+class TestServices(TestCase):
+    """Test GAE services."""
+
+    def setUp(self):
+        """Create tester instance."""
+        super.setUp(self)
+
+        from test_helper_gae3 import TestGae3
+
+        self.tester = TestGae3()
+        self.tester.set_up()
+
+    def tearDown(self):
+        """Deactivate tester."""
+        self.tester.tear_down()
+        super().tearDown()
+
+    def test_check_db_tables(self):
+        """Method check_db_tables."""
