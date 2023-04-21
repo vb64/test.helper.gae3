1fae4881e451259ee0649935ae6f68f39d9681f8
Vitaly Bogomolov <mail@vitaly-bogomolov.ru>
2023-04-21 13:50:17 +0400

    step

--- tests/test/test_services/test_task_queue.py
+++ tests/test/test_services/test_task_queue.py
@@ -14,12 +14,10 @@ class TestTaskQueue(TestServices):
         """Queue with one Task."""
         super().setUp()
         self.queue = Queue('default')
+        self.queue.add(Task('xxx', url='/'))
 
     def test_queue(self):
         """Check for number of tasks in queue."""
-        assert not self.tester.gae_tasks()
-
-        self.queue.add(Task('xxx', url='/'))
         self.tester.assert_tasks_num(1)
         tasks = self.tester.gae_tasks(queue_name='default', flush_queue=False)
 
@@ -33,3 +31,14 @@ class TestTaskQueue(TestServices):
         with pytest.raises(AssertionError) as err:
             self.tester.assert_tasks_num(1)
         assert 'task count: 0' in str(err.value)
+
+    def test_dict(self):
+        """Get queue content as dict."""
+        data = self.tester.gae_tasks_dict()
+        assert len(data) == 1
+        assert 'task1' in data
+
+    def test_dump(self):
+        """Dump queue content."""
+        self.tester.gae_queue_dump()
+        self.tester.gae_queue_dump(fields=['name', 'url'])
